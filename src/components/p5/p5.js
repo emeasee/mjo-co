33c6ae336c70f80ead6d77584541a6d3a4b21ba9
Mac Oosthuizen <m@m-jo.co>
2021-06-14 22:29:25 +0100

    update sketch file with logo generator #14

--- src/components/p5/p5.js
+++ src/components/p5/p5.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Helmet } from "react-helmet"
 import { Container } from './p5.css';
 import { loadableP5 as P5Wrapper } from './loadable';
 import Sketch from './sketch';
@@ -7,6 +8,10 @@ export default class App extends Component {
   render() {
     if (typeof window !== 'undefined') {
       return (
+        <Helmet>
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/1.19.1/TweenMax.min.js"></script>
+            <script src="https://cdn.rawgit.com/MAKIO135/73e5cb2c456084a9c356f9c3f89f8803/raw/3f4d08bf3c88c326702eff34bb621019b1ae5581/gfont.min.js"></script>
+        </Helmet>
         <Container>
           <P5Wrapper sketch={Sketch} />
         </Container>
--- src/components/p5/sketch.js
+++ src/components/p5/sketch.js
@@ -1,22 +1,123 @@
 export default function Sketch(p5) {
-  let canvas;
+// Inspired by Yeohyun Ahn and Viviana Cordova: Type + Code: Processing For Designers 
+// https://issuu.com/jpagecorrigan/docs/type-code_yeohyun-ahn
+//Credit to MAKIO135
 
-  p5.setup = () => {
-    canvas = p5.createCanvas(100, 100);
-    p5.noStroke();
-  };
+var WHITE, RED;
 
-  p5.draw = () => {
-    let y = p5.color('coral');
+var params = {
+    fontIndex: 3,
+    font: googleFonts[ 3 ].name,
+    fontStyle: googleFonts[ 3 ].styles[ 0 ],
+    text: 'm',
+    textSize: 200,
+    textSizeRange: [ 18, 500 ],
+    number: 10,
+    numberRange: [ 2, 18 ],
+    distance: 90,
+    distanceRange: [ -100, 100 ],
+    angle: 320,
+    angleRange: [ 0, 360  ],
+    globalAngle: 50,
+    globalAngleRange: [ 0, 360  ],
+    random: true
+};
 
-    p5.fill(y);
-    p5.circle(45, 45, 65);
+var animParams = {
+    textSize: params.textSize,
+    number: params.number,
+    distance: params.distance,
+    angle: params.angle,
+    globalAngle: params.globalAngle
+};
 
-    if (p5.mouseIsPressed) {
-      p5.fill(p5.color('lightsalmon'));
-    } else {
-      p5.fill(p5.color('hsl(160, 100%, 50%)'));
+
+var font;
+
+function setup() {
+    createCanvas( 600, 600 );
+    font = loadFont( googleFontsBaseUrl + googleFonts[ params.fontIndex ].files[ 0 ].url, useFont );
+    
+    WHITE = color( '#ffffff' );
+    RED = color( '#FB3550' );
+
+    noStroke();
+    fill( RED.levels[ 0 ], RED.levels[ 1 ], RED.levels[ 2 ], 1000 );
+    textAlign( CENTER, CENTER );
+    randomize();
+}
+
+function draw() {
+    background( WHITE );
+    
+    if( params.random && frameCount % 1000 == 0 ){
+        randomize();
     }
-    p5.circle(p5.mouseX, p5.mouseY, 70, 70);
-  };
+    
+    textSize( animParams.textSize );
+    translate( width/2, height/2 );
+    rotate( radians( animParams.globalAngle ) );
+    
+    for( var i = 0; i < animParams.number; i ++ ){
+        push();
+        rotate( i * TAU / animParams.number );
+        translate( 0, - map( animParams.distance, 0, 100, 0, height / 2 - animParams.textSize / 2 ) );
+        rotate( radians( animParams.angle ) );
+        text( params.text, 0, 0 );
+        pop();
+    }
+}
+
+function animate( value, _t ){
+    var time = _t || 0.5;
+    TweenMax.to( animParams, time, {
+        textSize: params.textSize,
+        number: params.number,
+        distance: params.distance,
+        angle: params.angle,
+        globalAngle: params.globalAngle
+    } );
+}
+
+function randomize(){
+    params.fontIndex = ~~ random( googleFonts.length ) 
+    params.font = googleFonts[ params.fontIndex ].name;
+    fontControllerUpdate( params.font );
+    animParams.text = params.text = String.fromCharCode( 65 + ~~random( 26 ) );
+    params.textSize = ~~ random( params.textSizeRange[ 0 ], params.textSizeRange[ 1 ] );
+    params.number = ~~ random( params.numberRange[ 0 ], params.numberRange[ 1 ] );
+    params.distance = ~~ random( params.distanceRange[ 0 ], params.distanceRange[ 1 ] );
+    params.angle = ~~ random( params.angleRange[ 0 ], params.angleRange[ 1 ] );
+    params.globalAngle = ~~ random( params.globalAngleRange[ 0 ], params.globalAngleRange[ 1 ] );
+    animate( 0, 1 );
+}
+
+function useFont(){
+    textFont( font );
+}
+
+function fontStyleControllerUpdate( value ){
+    for( let i = 0; i < googleFonts[ params.fontIndex ].files.length; i ++ ){
+        if( googleFonts[ params.fontIndex ].files[ i ].style == value ){
+            font = loadFont( googleFontsBaseUrl + googleFonts[ params.fontIndex ].files[ i ].url, useFont );
+            break;
+        }
+    }
+}
+
+function fontControllerUpdate( value ){
+    for( let i = 0; i < googleFonts.length; i++ ){
+      if( googleFonts[ i ].name == value ){
+            params.fontIndex = i;
+            break;
+        }
+    }
+    
+    for( let i = 0; i < googleFonts[ params.fontIndex ].files.length; i ++ ){
+        if( googleFonts[ params.fontIndex ].files[ i ].style == googleFonts[ params.fontIndex ].styles[ 0 ] ){
+            font = loadFont( googleFontsBaseUrl + googleFonts[ params.fontIndex ].files[ i ].url, useFont );
+            break;
+        }
+    }
+    }
 }
